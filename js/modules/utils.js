7617d081f3c257fd20f76ac96e8f8c2a09842b08
Ndikaaa <andiikapratamaa7@gmail.com>
2026-08-22 14:14:47 +0700

    Remove comment from utils.js
    Removed comment about shared helpers in utils.js.

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -1,4 +1,3 @@
-// Small shared helpers used across modules.
 export const qs = (sel, ctx = document) => ctx.querySelector(sel);
 export const qsa = (sel, ctx = document) => Array.from(ctx.querySelectorAll(sel));
 
